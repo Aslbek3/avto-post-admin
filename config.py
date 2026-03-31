5c2b3c81a9c6bb08b28510c21676f2cf724ff5b8
Aslbek <aslbekno07@gmail.com>
2026-03-31 16:30:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,14 @@ import pytz
 # SIZNING YANGI BOT TOKENINGIZNI SHU YERGA QO'YING:
 BOT_TOKEN = "8652336597:AAF6XzAsDfL1OWQ5IdhD9DVCYTlLsmHUI_8"
 
-ADMIN_ID = 7186161946  # O'zingizning Telegram ID raqamingiz
+ADMINS = [7586510077, 7186161946] 
 
 # MongoDB ulanish kodi (dollar belgisiz parol bilan)
 MONGO_URL = "mongodb+srv://usa2001aqsh_db_user:Asl2007@cluster0.sf2njxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 TIMEZONE = pytz.timezone("Asia/Tashkent")
   
+
+
+
+
