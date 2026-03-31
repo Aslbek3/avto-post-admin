b9426c2805d596c38ee61df389f6141b96bd3c8e
Aslbek <aslbekno07@gmail.com>
2026-03-31 11:04:50 +0500

    Update ADMIN_ID in config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import pytz
 # SIZNING YANGI BOT TOKENINGIZNI SHU YERGA QO'YING:
 BOT_TOKEN = "8652336597:AAF6XzAsDfL1OWQ5IdhD9DVCYTlLsmHUI_8"
 
-ADMIN_ID = 7586510077  # O'zingizning Telegram ID raqamingiz
+ADMIN_ID = 7186161946  # O'zingizning Telegram ID raqamingiz
 
 # MongoDB ulanish kodi (dollar belgisiz parol bilan)
 MONGO_URL = "mongodb+srv://usa2001aqsh_db_user:Asl2007@cluster0.sf2njxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
