a56bf3c5f742c13ea2fde8f824a028895cbda101
Aslbek <aslbekno07@gmail.com>
2026-03-31 10:30:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import pytz
 
 # SIZNING YANGI BOT TOKENINGIZNI SHU YERGA QO'YING:
-BOT_TOKEN = "YANGI_TOKEN_SHU_YERDA_BO'LADI"
+BOT_TOKEN = "8652336597:AAF6XzAsDfL1OWQ5IdhD9DVCYTlLsmHUI_8"
 
-ADMIN_ID = 123456789  # O'zingizning Telegram ID raqamingiz
+ADMIN_ID = 7586510077  # O'zingizning Telegram ID raqamingiz
 
 # MongoDB ulanish kodi (dollar belgisiz parol bilan)
 MONGO_URL = "mongodb+srv://usa2001aqsh_db_user:Aslbek2007@cluster0.sf2njxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
