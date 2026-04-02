abdeddd7cfbe1635fac387e49ff476bb5feb6d10
Aslbek <aslbekno07@gmail.com>
2026-04-02 12:54:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import pytz
 # SIZNING YANGI BOT TOKENINGIZNI SHU YERGA QO'YING:
 BOT_TOKEN = "8652336597:AAF6XzAsDfL1OWQ5IdhD9DVCYTlLsmHUI_8"
 
-ADMINS = [7586510077, 8117190758,  7186161946] 
+ADMINS = [7586510077, 7126035479,  7186161946] 
 
 # MongoDB ulanish kodi (dollar belgisiz parol bilan)
 MONGO_URL = "mongodb+srv://usa2001aqsh_db_user:Asl2007@cluster0.sf2njxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
