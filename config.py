60479ac892e09e0a439edd0a1eaef5bc87dd66f3
Aslbek <aslbekno07@gmail.com>
2026-04-05 11:07:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BOT_TOKEN = "8652336597:AAF6XzAsDfL1OWQ5IdhD9DVCYTlLsmHUI_8"
 ADMINS = [7586510077, 7126035479, 8424467038, 7186161946] 
 
 # MongoDB ulanish kodi (dollar belgisiz parol bilan)
-MONGO_URL = "mongodb+srv://usa2001aqsh_db_user:Asl2007@cluster0.sf2njxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URL = "mongodb+srv://usa:Asl2007@cluster0.sf2njxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 TIMEZONE = pytz.timezone("Asia/Tashkent")
   
